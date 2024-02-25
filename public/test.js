b27de0510ce4f52529d1e7cfe4be39871b15845a
Jasper <jaspergriffinjsg@hotmail.co.uk>
2024-02-25 21:52:40 +0000

    Deleted files after knowing viewBox SVG is required.

--- public/test.js
+++ public/test.js
@@ -1,7 +1,6 @@
 console.log("this is running");
 var a = document.getElementById("svg");
 
-
         //mouse wheel zoom
         //https://stackoverflow.com/questions/76150884/how-to-use-the-mouse-wheel-to-zoom-on-an-svg-using-the-viewbox
 
@@ -13,22 +12,39 @@ var a = document.getElementById("svg");
                 console.log("hover")
             })
 
-            rmd.addEventListener("mousedown", function() {
+            rmd.addEventListener("mousedown", function(e) {
                 rmd.setAttribute("style", "fill:red;fill-opacity:1;fill-rule:nonzero;stroke:none");
-                
+            
                 var svg2 = svgDoc.getElementById("svg2");
-                //original: viewBox="0 0 3408 2249.9999"
-                svg2.setAttribute("viewBox","700 500 1000 1000");
                 
-                a.classList.add('show');
+                let p = toSVGPoint(e.target, e.x, e.y);
+                let coord = `x: ${p.x} - y: ${p.y}`;
+                console.log(coord); 
+               
+                //https://www.sitepoint.com/how-to-translate-from-dom-to-svg-coordinates-and-back-again/
+                /*
+                var e = evt.target;
+                var dim = e.getBoundingClientRect();
+                var x = evt.clientX - dim.left;
+                var y = evt.clientY - dim.top;
+                alert("x: "+x+" y:"+y);
+                */
 
-                         
+                //(min-x, min-y, width, height)
+                //original: viewBox="0 0 3408 2249.9999"
+                
+                svg2.setAttribute("viewBox","-0.9 -13 3408 2249.9999");
+                
+                
         })
 
         })
 
 
-        
+        const toSVGPoint = (svg, x, y) => {
+            let p = new DOMPoint(x, y);
+            return p.matrixTransform(svg.getScreenCTM().inverse());
+          };    
 
 //try and pass the id of the station into this function
 function myFunc() {
