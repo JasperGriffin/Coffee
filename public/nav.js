41d02524157536caafef39d76240b3c8ebb13c99
Jasper <jaspergriffinjsg@hotmail.co.uk>
2024-07-06 16:05:21 +0100

    Updated svg map

--- public/nav.js
+++ public/nav.js
@@ -18,7 +18,7 @@ a.addEventListener("load", function() {
     svgCanvas.addEventListener('pointerdown', function (event) {
         drag = true;
         offset = { x: event.offsetX, y: event.offsetY };
-        console.log(event.offsetX + ", " + event.offsetY);
+        //console.log(event.offsetX + ", " + event.offsetY);
     });
 
     svgCanvas.addEventListener('pointermove', function (event) {
@@ -54,23 +54,7 @@ a.addEventListener("load", function() {
         drag = false;
     });
 
-    /*  
-    svgCanvas.addEventListener('wheel', function (event) {
-        var zoom = event.deltaY > 0 ? -1 : 1;
-        var scale = 1 + factor * zoom;
-        offset = {
-            x: event.offsetX,
-            y: event.offsetY
-        };
-        matrix.preMultiplySelf(new DOMMatrix()
-            .translateSelf(offset.x, offset.y)
-            .scaleSelf(scale, scale)
-            .translateSelf(-offset.x, -offset.y));
-        //viewPort.style.transform = matrix.toString();
-        viewPort.transform = matrix.toString();
 
-    });
-    */
     })
 
 
--- public/test.js
+++ public/test.js
@@ -4,40 +4,36 @@ var a = document.getElementById("svg");
         //mouse wheel zoom
         //https://stackoverflow.com/questions/76150884/how-to-use-the-mouse-wheel-to-zoom-on-an-svg-using-the-viewbox
 
+        
+
         a.addEventListener("load", function() {
             var svgDoc = a.contentDocument;
-            var rmd = svgDoc.getElementById("path4878");
+            var rmd = svgDoc.getElementById("lul-circle_lul-district_940gzzlubwt");
 
             rmd.addEventListener("mouseover", function() {
                 console.log("hover")
             })
 
             rmd.addEventListener("mousedown", function(e) {
-                rmd.setAttribute("style", "fill:red;fill-opacity:1;fill-rule:nonzero;stroke:none");
+                rmd.setAttribute("style", "fill:green;fill-opacity:1;fill-rule:nonzero;stroke:none");
             
                 var svg2 = svgDoc.getElementById("svg2");
                 
-                let p = toSVGPoint(e.target, e.x, e.y);
-                let coord = `x: ${p.x} - y: ${p.y}`;
-                console.log(coord); 
                
-                //https://www.sitepoint.com/how-to-translate-from-dom-to-svg-coordinates-and-back-again/
-                /*
-                var e = evt.target;
-                var dim = e.getBoundingClientRect();
-                var x = evt.clientX - dim.left;
-                var y = evt.clientY - dim.top;
-                alert("x: "+x+" y:"+y);
-                */
+                //getCoords(); 
 
-                //(min-x, min-y, width, height)
-                //original: viewBox="0 0 3408 2249.9999"
-            
+
+                
+                offset = { x: e.offsetX, y: e.offsetY };
+                console.log("coords: " + e.offsetX + ", " + e.offsetY);
+               
+                gsap.ticker.fps(-1); 
+
                 gsap.to(svg2, {
                     attr: {
-                        viewBox: '650 1450 200 200'
+                        viewBox: e.offsetX + ' ' + (e.offsetY-100) + ' 200 200' //'650 1450 200 200' - richmond
                     },
-                    duration: 1,
+                    duration: 4,
                     ease: "power3.inOut"
                    
                 })                
@@ -55,11 +51,13 @@ var a = document.getElementById("svg");
             return p.matrixTransform(svg.getScreenCTM().inverse());
           };    
 
-//try and pass the id of the station into this function
+
+/*try and pass the id of the station into this function
 function myFunc() {
     var a = document.getElementById("svg");
     var svgDoc = a.contentDocument;
     var svgItem = svgDoc.getElementById("path3322-8");
-    svgItem.setAttribute("style", "fill:red;fill-opacity:1;fill-rule:nonzero;stroke:none");
+    svgItem.setAttribute("style", "fill:green;fill-opacity:1;fill-rule:nonzero;stroke:none");
 
 }
+*/
